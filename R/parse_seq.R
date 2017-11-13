7f25dcaec56efbf3aedbe6a89c00604248033f50
franticspider <sjh518@york.ac.uk>
2017-11-13 22:49:55 +0000

    parse_seq handles missed cleaves now

--- R/parse_seq.R
+++ R/parse_seq.R
@@ -8,45 +8,81 @@
 #' @param massmin the minimum mass that a peptide should be. Defaults to 800
 #' @param massmax the maximum mass that a peptide should be. Defaults to 3500
 #' @param verbose whether to print messages to console during processing. Defaults to FALSE
+#' @param max.missed.cleaves the number of missed cleaves to incorporate in the set of peptides.
 #' @export
 #' @examples
 #' parse.seq("GPPGQKGPPGPQGPRGPPGPPGPM")
-#' spots  <- c("G7","G10","G13")
-#' sample <- "C1"
-parse.seq <- function(sequence,cuts="K|R",skip="X", massmin = 800, massmax = 3500,verbose = F){
-
+parse.seq <- function(sequence,cuts="K|R",skip="X", massmin = 800, massmax = 3500,verbose = F,max.missed.cleaves=0){
+  
+  #initialise some variables
   peptides <- NA
   pepidx <- 1
-
   len <- str_length(sequence)
-
   start <-1
-
-  for(pos in 1:len){
-    #if(verbose)message(sprintf("pos %d",pos))
+  nextpos <- start
+  num.mc <- 0
+  tooheavy <- F
+  badchar <- F
+  
+  attheend<-F
+  pos <-1
+  #for(pos in 1:len){
+  while(!attheend){ 
+   
+    #get the current amino acid
     aa <- str_sub(sequence,pos,pos)
+    
+    # if we are at a cut point, we can process the peptide, but note that
+    # we have to keep track of the start position for the *next* peptide
+    # which can get complicated as we deal with missed cleaves etc. 
     if(str_detect(aa,cuts)){
+      
+      #record the position of the next start sequence
+      #if(num.mc == 0)
+      nextpos[num.mc+1] = pos
+      
+      #increment the number of missed cleaves
+      num.mc <- num.mc + 1
+      
+      #get the current peptide sequence. 
       ss <- str_sub(sequence,start,pos)
-
+      
+      #if there are any characters that should be skipped
       if(str_detect(ss,skip)){
+        
+        #do nothing
         if(verbose)message(sprintf("  can't use %s",ss))
+        badchar <- T
       }
+      
+      #if there are no characters that should be skipped
       else{
-        #masses <- ms_tpeaks(ss)
+        
+        if(verbose)message(sprintf("found sequence %s",ss))
+        
+        #calculate the masses without PTMs
         masses <- ms_iso(ss)
+        tooheavy <- F
+        
+        #if the masses are in range
         if(masses$mass[1]>massmin && masses$mass[1] < massmax){
-
+          
+          #get the number of possible hydroxylations
           nhyd <- str_count(ss,"P")
+          
+          #get the number of possible deamidations
           nglut <- str_count(ss,"Q") + str_count(ss,"N")
-
+          
           #TODO: We have no data on the probabilities of hydroxylation atm...
-
+          
+          #Go through each hydroxylation / deamidation combination
           for(hh in 0:nhyd){
             for(dd in 0:nglut){
-
-
-        	  masses <- ms_iso(ss,ndeamidations=dd,nhydroxylations=hh)
-
+              
+              #calculate the mass for this PTM
+              masses <- ms_iso(ss,ndeamidations=dd,nhydroxylations=hh)
+              
+              #create the new entry in the peptide list
               newrow <- data.frame(
                 seq=as.character(ss)
                 ,nhyd=hh
@@ -54,8 +90,10 @@ parse.seq <- function(sequence,cuts="K|R",skip="X", massmin = 800, massmax = 350
                 #,mass1 = masses$mass[1] + (dd*0.984015)+(hh*16)
                 ,mass1 = masses$mass[1]
                 ,seqpos = start
+                ,missed.cleaves = num.mc -1 # we have to subtract 1 because we've incremented num.mc already
               )
-
+              
+              #add the row to the list
               if(pepidx==1){
                 peptides <- newrow
               }
@@ -63,24 +101,45 @@ parse.seq <- function(sequence,cuts="K|R",skip="X", massmin = 800, massmax = 350
                 peptides <- rbind(peptides,newrow)
               }
               pepidx <- pepidx+1
+              
+              
             }
           }
-
-
-          if(verbose)message(sprintf("found sequence %s",ss))
+          
         }
         else{
           if(verbose)message(sprintf("  mass %0.2f out of range for sequence %s",masses$mass[1],ss))
+          if(masses$mass[1] > massmax)
+            tooheavy <- T
         }
       }
-      start <- pos + 1
+      #if we are at the limit of mass or num.missed.cleaves, move the start pos on
+      # or if too heavy
+      # or if there's a bad character
+      if(tooheavy || badchar || num.mc > max.missed.cleaves){
+        start <- nextpos[1]+1
+        tooheavy <- F
+        badchar <- F
+        #rewind the position if we've missed a cleave
+        if(num.mc > 0)
+          pos <- nextpos[1]#this will be incremented below!
+        
+        if(verbose)
+          message(sprintf("nmc = %0.0f, start =%0.0f, pos = %0.0f\ncleave positions are %0.0f %0.0f %0.0f",num.mc,start,pos,nextpos[1],nextpos[2],nextpos[3]))
+
+        num.mc <- 0
+      }
     }
-
-
+    
+    pos <- pos + 1
+    
+    if(pos>len)
+      break
+    
   }
-
+  
   #TODO: demistify why we have to use as.character for sequences...
   peptides$seq <- as.character(peptides$seq)
   return(peptides)
-
+  
 }
---
+++ R/xy_pos_prolines.R
@@ -0,0 +1,46 @@
+#' count the prolines in the x and y position of a collagen peptide
+#' @param seq the amino acid sequence
+#' @param verbose default is False
+#' @keywords isotopes mass sequence deamidation hyroxylation
+#' @export
+#' @examples
+#' xy.pos.prolines("IGQPGAVGPAGIR")
+xy.ypos.prolines <- function(seq,verbose = F){
+  
+  gcount <- vector(length = 3)
+  pcount <- vector(length = 3)
+
+  if(verbose)
+    message(sprintf("Sequence is %d long",length(seq)))
+            
+  for(ii in 1:nchar(seq)){
+    
+    pos <- (ii%%3)+1
+    
+    if(substr(seq,ii,ii) == "G")
+      gcount[pos] = gcount[pos] + 1#str_count(seq[ii],"G")
+    
+    if(substr(seq,ii,ii) == "P")
+      pcount[pos] = pcount[pos] + 1#str_count(seq[ii],"P")
+    
+    #if(verbose){
+    #  message(sprintf("ii = %d, pos %d, letter = %s, gcount = %d %d %d, pcount = %d %d %d",ii,pos,
+    #                  substr(seq,ii,ii),
+    #                  gcount[1],gcount[2],gcount[3],
+    #                  pcount[1],pcount[2],pcount[3]))
+    #}
+  }
+  
+  gmax = which.max(gcount)
+  ypos = (gmax+1)%%3+1
+  
+  if(verbose){
+    message(sprintf("sequence: %s",seq))
+    message(sprintf("G count positions: %d %d %d, max %d",gcount[1],gcount[2],gcount[3],gmax))
+    message(sprintf("P count positions: %d %d %d, yct %d",pcount[1],pcount[2],pcount[3],ypos))
+  }
+  
+  return(pcount[ypos])
+}
+
+
