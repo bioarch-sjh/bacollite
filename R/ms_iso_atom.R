f99fb98c3567553a304e6e72dde886bdccba593d
bioarch-sjh <simon.hickinbotham@york.ac.uk>
2018-03-05 17:31:21 +0000

    Update ms_iso_atom.R
    added so that we can calculate isotopes of molecules via counts of C,H,N,O,S

--- R/ms_iso_atom.R
+++ R/ms_iso_atom.R
@@ -1,1 +1,32 @@
 
+#' Obtain the isotope distribution for a molecule consisting of atoms of C,H,N,O and S
+#' post-translation modifications (deamitation and hydroxylation)
+#' @param atoms vector containing the counts of atoms in the order C,H,N,O and S
+#' @keywords isotopes mass sequence deamidation hyroxylation
+#' @export
+#' @examples
+#' ms_iso_atom(atoms)
+#' ms_iso_atom(c(47,69,15,12,0))
+ms_iso_atom <- function(atoms,verbose=F){
+
+  if(length(atoms)< 5){
+    message("ms_iso_atom error: Vector with five entries needed\nreturning NA")
+    return NA
+    }
+  if(is.numeric(atoms) == F){
+    message("ms_iso_atom error: Vector with five integers needed\nreturning NA")
+    return NA
+    }
+    
+  
+  
+  if(verbose){
+    message(sprintf("Calculating isotope distributions"))
+  }
+
+  result <- cppIsoAtom_p(atoms)
+
+
+  return (result)
+
+}
