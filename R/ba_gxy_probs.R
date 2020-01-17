b571fd4b00ec14ccaa4997913efa7ee0a7b83131
Simon Hickinbotham <sjh518@york.ac.uk>
2020-01-17 15:23:16 +0000

    Modified ba_gxy_probs to flag and ignore seqences that don't have the GXY motif

--- R/ba_gxy_probs.R
+++ R/ba_gxy_probs.R
@@ -12,12 +12,12 @@
 #' @examples
 #' ba_gxy_probs
 ba_gxy_probs <- function(seq, xprob = 0.05, yprob = 0.95, xyxprob = 0.5, xyyprob = 0.99, verbose = F){
-
+  #message("DEBUGGING!")
   if(verbose)
     message("inside ba_gxy_probs")
-  
+
   nohyd <- data.frame(prob=1,nhyd=0)
-  
+
   #ERROR CHECKING
   if(str_detect(seq,"G") == F){
     if(verbose)
@@ -29,21 +29,21 @@ ba_gxy_probs <- function(seq, xprob = 0.05, yprob = 0.95, xyxprob = 0.5, xyyprob
       message("No P in this sequence - can't determine any X-Y proline positions\nReturning 0 hydroxylation prob of 1")
     return (nohyd)
   }
-  
+
   pclass <- c("X","Y")
 
   if(verbose)
     message(sprintf("Processing seq %s",seq))
-  
+
   #figure out the G position - store it in gpos (NB - may need a strategy for the nonmodal G positions, possibly only process GXYG motifs..?)
   gs <- as.data.frame(str_locate_all(seq,"G"))
   # get the position value
   gm3 <- gs$start%%3
   # get the unique positions (should be 0 1 or 2)
   ux <- as.integer(unique(gm3))
-  # get the modal position: 
+  # get the modal position:
   gpos <- ux[which.max(tabulate(match(gm3,ux)))]
-  
+
   if(verbose)
     message(sprintf("Gs at position %d",gpos))
 
@@ -66,41 +66,48 @@ ba_gxy_probs <- function(seq, xprob = 0.05, yprob = 0.95, xyxprob = 0.5, xyyprob
       pmod <- ppos%%3
       pidx <- NA
 
-      if(gpos == 0){#pmod can be 1 or 2
-        if(pmod == 1)pidx <- 1 #"X"
-        if(pmod == 2)pidx <- 2 #"Y"
+      message(sprintf("Gpos is %d, pmod is %d",gpos,pmod))
+      if(gpos == pmod){
+        if(verbose)
+          message(sprintf("Warning! P at position %d is out of sync with GXY",ps$start[pp]))
       }
-      if(gpos == 1){#pmod can be 0 or 2
-        if(pmod == 2)pidx <- 1 #"X"
-        if(pmod == 0)pidx <- 2 #"Y"
-      }
-      if(gpos == 2){#pmod can be 0 or 1
-        if(pmod == 0)pidx <- 1 #"X"
-        if(pmod == 1)pidx <- 2 #"Y
-      }
+      else{
+        if(gpos == 0){#pmod can be 1 or 2 - CAN'T BE 0!
+          if(pmod == 1)pidx <- 1 #"X"
+          if(pmod == 2)pidx <- 2 #"Y"
+        }
+        if(gpos == 1){#pmod can be 0 or 2
+          if(pmod == 2)pidx <- 1 #"X"
+          if(pmod == 0)pidx <- 2 #"Y"
+        }
+        if(gpos == 2){#pmod can be 0 or 1
+          if(pmod == 0)pidx <- 1 #"X"
+          if(pmod == 1)pidx <- 2 #"Y
+        }
 
-      if(pidx == 1)
-        ps$prob[pp]<-xprob
-      if(pidx == 2)
-        ps$prob[pp]<-yprob
-      
-      if(verbose)
-        message(sprintf("P found at %d, pmod is %d, pclass is %s, prob is %f",ppos,pmod,pclass[pidx],ps$prob[pp]))
+        if(pidx == 1)
+          ps$prob[pp]<-xprob
+        if(pidx == 2)
+          ps$prob[pp]<-yprob
 
-      #SEE IF THERE'S A PROLINE AT THE OTHER POSITION
-      if(pidx == 1){#X position
-        if(ppos < str_length(seq)){
-          if(verbose)
-            message(sprintf("Y posn available, n ppos = %d, sequence is %s",length(ppos),seq))
-          
-          if(str_sub(seq,ppos+1,ppos+1) == "P"){
+        if(verbose)
+          message(sprintf("P found at %d, pmod is %d, pclass is %s, prob is %f",ppos,pmod,pclass[pidx],ps$prob[pp]))
+
+        #SEE IF THERE'S A PROLINE AT THE OTHER POSITION
+        if(pidx == 1){#X position
+          if(ppos < str_length(seq)){
             if(verbose)
-              message(sprintf("pp = %d: GPP motif found at position %d",pp,ppos))
-          
-            PPmotif <- T
-            ps$prob[pp]<-xyxprob
-            ps$prob[(pp+1)]<-xyyprob
+              message(sprintf("Y posn available, n ppos = %d, sequence is %s",length(ppos),seq))
 
+            if(str_sub(seq,ppos+1,ppos+1) == "P"){
+              if(verbose)
+                message(sprintf("pp = %d: GPP motif found at position %d",pp,ppos))
+
+              PPmotif <- T
+              ps$prob[pp]<-xyxprob
+              ps$prob[(pp+1)]<-xyyprob
+
+            }
           }
         }
       }
@@ -110,10 +117,10 @@ ba_gxy_probs <- function(seq, xprob = 0.05, yprob = 0.95, xyxprob = 0.5, xyyprob
   #TODO: print ps if(verbose)
   #print(ps)
 
-  # Now we have assigned hydroxylation probabilities to each proline in the sequence, we can calculate the 
+  # Now we have assigned hydroxylation probabilities to each proline in the sequence, we can calculate the
   # probability of each hydroxylation level
   nhyd <- ba_nhyd(ps$prob,verbose = F)
-  
+
   return(nhyd)
 
 }
